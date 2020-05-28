e99f2b003c8e3a52bc1c602392521d2032b687ea
user <user@DESKTOP-PLHOS04>
2020-05-29 00:04:53 +0900

    add

--- ProjectjavaFx/src/Fxml/Appmain.java
+++ ProjectjavaFx/src/Fxml/Appmain.java
@@ -1,5 +1,6 @@
 package Fxml;
 
+import java.awt.event.WindowEvent;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.collections.ObservableList;
@@ -17,31 +18,50 @@ public class Appmain extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		HBox root = new HBox();
-		root.setPadding(new Insets(50, 50, 50, 50)); // 컨트롤하고 간격을 패딩 감싸고 있는 H박스 2개가 있는데 버튼박스와의 간격을 패딩
-		root.setSpacing(200); // 컨트롤과의 간격
-
+		root.setPadding(new Insets(10, 10, 10, 10)); // 컨트롤하고 간격을 패딩 감싸고 있는 H박스 2개가 있는데 버튼박스와의 간격을 패딩
+		root.setSpacing(10); // 컨트롤과의 간격
+		root.setPrefSize(700, 500);
 		TextField textField = new TextField();
 		textField.setPrefWidth(200);
-//		textField.setPrefHeight(200);
+		textField.setPrefHeight(200);
 
-		Button button = new Button();
-		button.setText("확인");
-		button.setOnAction(new EventHandler<ActionEvent>() {
+		Button button1 = new Button();
+		button1.setText("확인");
+		button1.setPrefSize(100, 100);
+		button1.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
-				Platform.exit();
+				textField.setText("확인을 눌렀습니다.");
+//				Platform.exit();
+			}
+		});
+
+		Button button2 = new Button();
+		button2.setText("취소");
+		button2.setPrefSize(300, 500);
+		button2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				textField.setText(null);
+//				Platform.exit();
 			}
 		});
 
 //		ObservableList list = root.getChildren();
 		root.getChildren().add(textField);
-		root.getChildren().add(button);
+		root.getChildren().add(button1);
+		root.getChildren().add(button2);
 
 		Scene scene = new Scene(root);
 
 		primaryStage.setTitle("AppMain");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-	}
+//		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+//			public void handle(WindowEvent event) {
+//				System.out.println(event);
+//			}
+//		});
+	} 
 
 	public static void main(String[] args) {
 		launch(args);
